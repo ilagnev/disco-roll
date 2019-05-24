7ccc0be18dedb26c583b0e21dab633ae80381380
ilagnev <ilagnev@gmail.com>
2019-05-24 19:06:50 +0300

    add some dev code to work with hardware

--- index.js
+++ index.js
@@ -1,30 +1,75 @@
 const http = require('https');
+const url = require('url');
 
-const url = process.env['SITE_URL'];
 const interval = 5000;
+const siteUrl = process.env['SITE_URL'];
+let urlObj;
+let = isBall = false;
+let = isDebug = false;
 
-if (!url) {
+if (!siteUrl) {
 	console.error('SITE_URL not defined');
 	process.exit(-2);
+} else {
+	urlObj = url.parse(siteUrl);
+	console.log(`URL to watch: ${siteUrl}`, { urlObj });
+}
+
+const {
+	turnOnBall, turnOffBall,
+	turnOnDebug, turnOffDebug,
+	isBallOn, isDebugOn,
+} = require('./tumblers');
+
+const requestParams = {
+	method: 'HEAD',
+	protocol: urlObj.protocol,
+	host: urlObj.host,
+  port: urlObj.port,
+  path: urlObj.path,
 }
 
 const isCexAlive = () => {
-	var request = http.request(url, function (res) {
-		var data = '';
-		res.on('data', chunk => data += chunk);
+	const request = http.request(requestParams, (res) => {
+
+		console.log('not ended date:', new Date());
+		console.log('not ended statusCode:', res.statusCode);
+		// let data = '';
+		// res.on('data', chunk => data += chunk);
+
 		res.on('end', () => {
 			console.log(new Date());
-			// console.log(data.trim());
 			console.log(res.statusCode);
+			console.log('');
+			// console.log(data.trim());
 			// console.log(res.headers);
-			console.log("\n");
 		});
 	});
-	request.on('error', function (e) {
-		console.log(e);
+
+	request.on('error', err => {
+		console.error(err);
+
+		// no internet connection
+		if (err.code === 'ENOTFOUND') {
+			//todo turn red diod bulb (write to some channel)
+			if (isDebug === false) {
+				isDebug = true;
+				turnOnDebug();
+			}
+		}
 	});
+
 	request.end();
 }
 
-isCexAlive();
-setInterval(() => isCexAlive(), interval);
+const init = async () => {
+	isBall = await isBallOn();
+	isDebug = await isDebugOn();
+
+	console.log('init: ', { isBall, isDebug });
+
+	isCexAlive();
+	setInterval(() => isCexAlive(), interval);
+}
+
+module.exports = init();
---
+++ tumblers.js
@@ -0,0 +1,24 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function runCommand(cmd) {
+  const { stdout, stderr } = await exec(cmd);
+  console.log(`${cmd}: stdout:${stdout}`);
+  console.log(`${cmd}: stderr:${stderr}`);
+
+	return stdout;
+}
+
+(async () => {
+	await runCommand('gpio mode 7 output');
+	await runCommand('gpio mode 21 output');
+})();
+
+module.exports = {
+	turnOnBall: () => runCommand('gpio write 7 1'),
+	turnOffBall: () => runCommand('gpio write 7 0'),
+	turnOnDebug: () => runCommand('gpio write 21 1'),
+	turnOffDebug: () => runCommand('gpio write 21 0'),
+	isBallOn: async () => await runCommand('gpio read 7') === '1',
+	isDebugOn: async () => await runCommand('gpio read 21') === '1',
+}
